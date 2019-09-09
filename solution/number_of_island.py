efbe7b64dfc582d52ed900aea4d4c72ac87e4587
HsiangChih Cheng <musicmilif@gmail.com>
2019-09-09 16:46:42 +0800

    [BUGFIX] fix visit and push

--- solution/number_of_island.py
+++ solution/number_of_island.py
@@ -34,35 +34,29 @@ class BFS():
         self.n_row = len(grid)
         self.n_col = len(grid[0])
         self.size = self.n_row * self.n_col
-        self.queue = [[0, 0] for i in range(self.size)]
+        self.queue = [[0, 0] for _ in range(self.size)]
         self.front = -1
         self.rear = -1
 
     def isEmpty(self):
-        if self.front == -1 and self.rear == -1:
-            return True
-        else:
-            return False
+        return self.front == self.rear == -1
 
     def isFull(self):
-        if self.rear == self.size - 1:
-            return True
-        else:
-            return False
+        return self.rear == self.size - 1
 
     def push(self, val: list):
         if self.isEmpty():
             self.front += 1
         if self.isFull():
             return False
-        self.rear = self.rear + 1
+        self.rear += 1
         self.queue[self.rear] = val
 
     def pop(self):
         if self.isEmpty():
             return False
         self.front += 1
-        if self.front + 1 == self.rear:
+        if self.front - 1 == self.rear:
             self.front = -1
             self.rear = -1
         return
@@ -81,36 +75,32 @@ class BFS():
         # push the root
         self.push([row, col])
         while not self.isEmpty():
-            cur = self.peek()
-            row, col = cur[0], cur[1]
-            val = self.grid[row][col]
+            row, col = self.peek()
             # mark been visited in queue
-            if not val == '0':
+            if self.grid[row][col] == '1':
+                self.grid[row][col] = '0'
                 # push the neighbor
                 for i, j in [(row + 1, col), (row, col + 1), (row - 1, col), (row, col - 1)]:
-                    if 0 <= i < self.n_row and 0 <= j < self.n_col and not self.grid[i][j] == '0':
-                        val = self.grid[i][j]
-                        self.grid[i][j] = '0'
-                        if val == '1':
-                            self.push([i, j])
-            self.pop()
+                    if 0 <= i < self.n_row and 0 <= j < self.n_col and self.grid[i][j] == '1':
+                        self.push([i, j])
+                self.pop()
+
         return self.grid
 
 
 class Solution:
     def numIslands(self, grid) -> int:
-        if len(grid) == 0:
+        if not grid or not grid[0]:
             return 0
-        n_row = len(grid)
-        n_col = len(grid[0])
+        n_row, n_col = len(grid), len(grid[0])
         n_island = 0
 
         for i in range(n_row):
             for j in range(n_col):
-                val = grid[i][j]
-                if not val == '0':
+                if grid[i][j] == '1':
                     bfs = BFS(grid)
                     grid = bfs.traverse(i, j)
+                    print('='*10)
                     n_island += 1
 
         return n_island
